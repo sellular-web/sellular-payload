5cfd21f932f58867efc766df989c7fb5939d6963
Avalynn <avalynndev@gmail.com>
2024-03-17 15:04:15 +0530

    Remove useless components.

--- src/payload/payload.config.ts
+++ src/payload/payload.config.ts
@@ -30,7 +30,7 @@ import { priceUpdated } from './stripe/webhooks/priceUpdated'
 import { productUpdated } from './stripe/webhooks/productUpdated'
 
 const generateTitle: GenerateTitle = () => {
-  return 'My Store'
+  return 'Sellular'
 }
 
 const mockModulePath = path.resolve(__dirname, './emptyModuleMock.js')
@@ -43,14 +43,6 @@ export default buildConfig({
   admin: {
     user: Users.slug,
     bundler: webpackBundler(), // bundler-config
-    components: {
-      // The `BeforeLogin` component renders a message that you see while logging into your admin panel.
-      // Feel free to delete this at any time. Simply remove the line below and the import `BeforeLogin` statement on line 15.
-      beforeLogin: [BeforeLogin],
-      // The `BeforeDashboard` component renders the 'welcome' block that you see after logging into your admin panel.
-      // Feel free to delete this at any time. Simply remove the line below and the import `BeforeDashboard` statement on line 15.
-      beforeDashboard: [BeforeDashboard],
-    },
     webpack: config => {
       return {
         ...config,
